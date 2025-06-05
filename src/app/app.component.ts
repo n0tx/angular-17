5972268c5a3c912c3f52bea421a43f5c0296a7b8
n0tx <rcandra91@msn.com>
2025-06-06 01:44:44 +0700

    menambahakan home component pada app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = "Hello I'm from AppComponent (<app-root>)";
+  title = "Hello I'm from AppComponent, <app-root>";
 }
