a9fb5e245de6fa7b0e5166d4818e4c899c8525b3
n0tx <rcandra91@msn.com>
2025-06-06 01:54:11 +0700

    data binding menggunakan signal() pada header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  headerMessage = signal('header works!');
 }
