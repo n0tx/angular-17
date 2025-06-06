d20dd63cccb2acab5b8acf12ecdf941072a2f730
n0tx <rcandra91@msn.com>
2025-06-07 01:36:59 +0700

    menambahkan logic increment, decrement, reset pada komponen counter menambahkan tampilan button, pesan dalam paragraf tag dan css style pada komponen counter

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
   imports: [],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.scss'
+  styleUrl: './counter.component.scss',
 })
 export class CounterComponent {
+  counterMessage = signal('counter works!');
 
+  counterValue = signal(0);
+
+  increment() {
+    this.counterValue.update((val) => val + 1);
+  }
+  decrement() {
+    this.counterValue.update((val) => val - 1);
+  }
+  reset() {
+    this.counterValue.set(0);
+  }
 }
