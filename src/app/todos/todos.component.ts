c8e18af6b085cfef7338c3b3715a30eb4cbb73bd
n0tx <rcandra91@msn.com>
2025-06-09 20:57:48 +0700

    generate todo-item dengan angular cli menambahkan todo-item ke komponen todo

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
