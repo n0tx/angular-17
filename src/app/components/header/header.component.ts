b56da4c8948f585e8caa610e6d13c79ca00dc482
n0tx <rcandra91@msn.com>
2025-06-09 19:33:25 +0700

    menambahkan todos link pada header mengatur header untuk tata letak navigasi home dan todoLink pada header.scss membuat signal text pada header.ts untuk judul Todos link yang akan ditampilkan header komponen

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   headerMessage = signal('My First Angular App');
+  todosLink = signal('Todos');
 }
